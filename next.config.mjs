69e1d8c228fc680eb81dbd7b88046c452c1461a2
manoachracon-cloud <manoach.racon@icloud.com>
2026-05-01 13:44:35 -0400

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  typescript: {
+    ignoreBuildErrors: true
+  },
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "www.estheticdiamonds.fr",
-        pathname: "/ressources/images/**"
+        hostname: "www.estheticdiamonds.fr"
+      },
+      {
+        protocol: "https",
+        hostname: "estheticdiamonds.fr"
       }
     ]
   }
